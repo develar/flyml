f921a242a14f21a9ccae3906b06eef5908003c7b
develar <develar@gmail.com>
2010-09-03 20:22:29 +0400

    continue ast generation (before clean experiment code)

--- src/main/java/org/flyti/javaBuilderFlex/ASTBuilder.java
+++ src/main/java/org/flyti/javaBuilderFlex/ASTBuilder.java
@@ -1,6 +1,5 @@
 package org.flyti.javaBuilderFlex;
 
-import flash.swf.tools.as3.PrettyPrinter;
 import flex2.compiler.as3.AbstractSyntaxTreeUtil;
 import macromedia.asc.parser.*;
 import macromedia.asc.semantics.NamespaceValue;
@@ -8,7 +7,7 @@ import macromedia.asc.semantics.ReferenceValue;
 import macromedia.asc.util.Context;
 import macromedia.asc.util.Namespaces;
 import macromedia.asc.util.ObjectList;
-import sun.tools.tree.ThisExpression;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,6 +26,8 @@ public class ASTBuilder extends AbstractBuilder {
     super(putBufferInMark);
   }
 
+  private static boolean s = true;
+
   public void build(File file, ProgramNode syntaxTree, Context context) throws FileNotFoundException {
     ObjectList<Node> items = syntaxTree.pkgdefs.get(0).statements.items;
     // ClassDefinition closer to end
@@ -44,6 +45,10 @@ public class ASTBuilder extends AbstractBuilder {
 
   private void createFunction(Context context, StatementListNode statements) {
     NodeFactory nodeFactory = context.getNodeFactory();
+
+//    NodeFactory nodeFactory = context.getNodeFactory();
+
+
     AttributeListNode attributeList = new AttributeListNode(null, -1);
     attributeList.items.remove(0);
 //    attributeList.hasOverride = true;
@@ -52,58 +57,63 @@ public class ASTBuilder extends AbstractBuilder {
 
     StatementListNode functionBody = new StatementListNode(null);
 
+    QualifiedIdentifierNode name = new QualifiedIdentifierNode(null, "v1", 0);
+    IdentifierNode type = createResolvedIdentifier(context, "flash.text", "TextField");
+
+    CallExpressionNode call = new CallExpressionNode(type, null);
+    call.is_new = true;
+
+    VariableBindingNode var = new VariableBindingNode(null, null, Tokens.VAR_TOKEN, new TypedIdentifierNode(name, createTypeMemberExpressionNode(type), 0), new MemberExpressionNode(null, call, 0));
+    functionBody.items.add(new VariableDefinitionNode(null, null, Tokens.VAR_TOKEN, new ListNode(null, var, 0), 0));
 
 
-//    TypedIdentifierNode type = new TypedIdentifierNode(new IdentifierNode("ss", false, -1), createTypeMemberExpressionNode(context, "flash.text", "TextField"), -1);
-    TypedIdentifierNode type = new TypedIdentifierNode(new IdentifierNode("ss", false, -1), createTypeMemberExpressionNode(context, "flash.text", "TextField"), -1);
+    GetExpressionNode getExpressionNode = new GetExpressionNode(new IdentifierNode("v1", false, 0));
+    getExpressionNode.setMode(Tokens.EMPTY_TOKEN);
+
+    GetExpressionNode getExpressionNode2 = new GetExpressionNode(new IdentifierNode("addChild", false, 0));
+    getExpressionNode.setMode(Tokens.EMPTY_TOKEN);
+
+    SetExpressionNode set = new SetExpressionNode(new IdentifierNode("text", 0), new ArgumentListNode(new LiteralStringNode("sss AEFLCWEHJCKSAX", false, false), 0));
+//    ExpressionStatementNode expressionStatementNode = new ExpressionStatementNode(new ListNode(null, new MemberExpressionNode(new MemberExpressionNode(null, getExpressionNode, 0), set, 0), 0));
+    ExpressionStatementNode expressionStatementNode = new ExpressionStatementNode(new MemberExpressionNode(new MemberExpressionNode(null, getExpressionNode, 0), set, 0));
+    functionBody.items.add(expressionStatementNode);
+
+    CallExpressionNode callMethod = new CallExpressionNode(new IdentifierNode("addChild", false, 0), new ArgumentListNode(new MemberExpressionNode(null, getExpressionNode, 0), 0));
+//    CallExpressionNode callMethod = new CallExpressionNode(getExpressionNode2, new ArgumentListNode(new MemberExpressionNode(null, getExpressionNode, 0), 0));
+    callMethod.setMode(Tokens.EMPTY_TOKEN);
+    callMethod.setRValue(true);
 
 
-//    ApplyTypeExprNode apply = new ApplyTypeExprNode(expr, typeArgs);
-//                apply.setRValue(true);
+//    if (statements.items.size() == 2  && s) {
+//      s = false;
+    functionBody.items.add(new ExpressionStatementNode(new ListNode(null, new MemberExpressionNode(null, callMethod, 0), 0)));
+//      functionBody.items.add(((FunctionDefinitionNode) statements.items.get(1)).fexpr.body.items.get(2));
+//      ASTUtil.dump(functionBody.items.last(), ((FunctionDefinitionNode) statements.items.get(1)).fexpr.body.items.get(2));
+//    }
 
-    CallExpressionNode call = new CallExpressionNode(new TypeExpressionNode(createTypeMemberExpressionNode(context, "flash.text", "TextField"), true, false), null);
-       call.is_new = true;
-    VariableBindingNode var = new VariableBindingNode(null, null, Tokens.VAR_TOKEN, type, new MemberExpressionNode(null, call, 0));
-//    functionBody.items.add();
+//    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, null, createChildrenFunctionSignature, functionBody, -1);
+//    functionCommon.setFunctionDefinition(true);
+//    FunctionDefinitionNode node = new FunctionDefinitionNode(context, nodeFactory.current_package, attributeList, createChildrenFunctionName, functionCommon);
 
-
-//    functionBody.items.add(new VariableDefinitionNode(null, null, Tokens.VAR_TOKEN, new ListNode(null, var, 0), 0));
-    functionBody.items.add(AbstractSyntaxTreeUtil.generateVariableNew(nodeFactory, "s", "flash.text.TextField", 0));
-
-    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, null, createChildrenFunctionSignature, functionBody, -1);
-    functionCommon.setFunctionDefinition(true);
+//    IdentifierNode identifier = new IdentifierNode("createChildren", false, -1);
+    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, createChildrenFunctionName.identifier, createChildrenFunctionSignature, functionBody, -1);
     FunctionDefinitionNode node = new FunctionDefinitionNode(context, nodeFactory.current_package, attributeList, createChildrenFunctionName, functionCommon);
     statements.items.add(node);
-
-//    PrintWriter printWriter;
-//    try {
-//      printWriter = new PrintWriter(new File("/Users/develar/t.as"));
-//    }
-//    catch (FileNotFoundException e) {
-//      e.printStackTrace();
-//      return;
-//    }
-//    PrettyPrinter prettyPrinter = new PrettyPrinter(printWriter);
-//    prettyPrinter.evaluate(context, statements);
-//    printWriter.flush();
-//    printWriter.close();
   }
 
-  private static MemberExpressionNode createTypeMemberExpressionNode(Context context, String packageName, String unqualifiedTypeName) {
-    GetExpressionNode getExpressionNode = new GetExpressionNode(generateResolvedIdentifier(context, packageName, unqualifiedTypeName));
+  private static MemberExpressionNode createTypeMemberExpressionNode(IdentifierNode identifier) {
+    GetExpressionNode getExpressionNode = new GetExpressionNode(identifier);
     getExpressionNode.setMode(Tokens.EMPTY_TOKEN);
     return new MemberExpressionNode(null, getExpressionNode, 0);
   }
 
-  private static IdentifierNode generateResolvedIdentifier(Context context, String namespace, String name) {
-    IdentifierNode result = new IdentifierNode(name, false, 0);
-    Namespaces namespaces = new Namespaces();
+  private static IdentifierNode createResolvedIdentifier(Context context, String namespace, String name) {
     NamespaceValue namespaceValue = new NamespaceValue();
     namespaceValue.name = namespace;
-    namespaces.add(namespaceValue);
-    ReferenceValue referenceValue = new ReferenceValue(context, null, name, namespaces);
-    referenceValue.setIsAttributeIdentifier(false);
-    result.ref = referenceValue;
-    return result;
+    Namespaces namespaces = new Namespaces(namespaceValue);
+
+    IdentifierNode identifier = new IdentifierNode(name, false, 0);
+    identifier.ref = new ReferenceValue(context, null, name, namespaces);
+    return identifier;
   }
 }
--- src/main/java/org/flyti/javaBuilderFlex/ASTUtil.java
+++ src/main/java/org/flyti/javaBuilderFlex/ASTUtil.java
@@ -1,5 +1,18 @@
 package org.flyti.javaBuilderFlex;
 
-public class ASTUtil {
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.*;
+
+public class ASTUtil {
+  public static void dump(Object a, Object b) {
+    Yaml yaml = new Yaml();
+    try {
+      yaml.dump(a, new FileWriter(new File("/Users/develar/a.yml")));
+      yaml.dump(b, new FileWriter(new File("/Users/develar/b.yml")));
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
