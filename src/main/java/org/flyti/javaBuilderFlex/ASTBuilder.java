4ba500763ce916669fc2b63300127b9364e1e123
develar <develar@gmail.com>
2010-09-03 20:31:43 +0400

    continue ast generation (works)

--- src/main/java/org/flyti/javaBuilderFlex/ASTBuilder.java
+++ src/main/java/org/flyti/javaBuilderFlex/ASTBuilder.java
@@ -1,25 +1,25 @@
 package org.flyti.javaBuilderFlex;
 
-import flex2.compiler.as3.AbstractSyntaxTreeUtil;
 import macromedia.asc.parser.*;
 import macromedia.asc.semantics.NamespaceValue;
 import macromedia.asc.semantics.ReferenceValue;
 import macromedia.asc.util.Context;
 import macromedia.asc.util.Namespaces;
 import macromedia.asc.util.ObjectList;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 
 public class ASTBuilder extends AbstractBuilder {
   private static final FunctionSignatureNode createChildrenFunctionSignature = new FunctionSignatureNode(null, null);
   private static final FunctionNameNode createChildrenFunctionName = new FunctionNameNode(Tokens.EMPTY_TOKEN, new IdentifierNode("createChildren", false, -1));
-//  private static final FunctionNameNode createChildrenQualifiedIdentifierNode = new FunctionNameNode(Tokens.EMPTY_TOKEN, new IdentifierNode("createChildren", false, -1));
+  private static final AttributeListNode attributeListPublic = new AttributeListNode(null, -1);
 
   static {
     createChildrenFunctionSignature.void_anno = true;
+
+    attributeListPublic.items.remove(0);
+    attributeListPublic.hasPublic = true;
   }
 
   public ASTBuilder(boolean putBufferInMark) {
@@ -46,15 +46,6 @@ public class ASTBuilder extends AbstractBuilder {
   private void createFunction(Context context, StatementListNode statements) {
     NodeFactory nodeFactory = context.getNodeFactory();
 
-//    NodeFactory nodeFactory = context.getNodeFactory();
-
-
-    AttributeListNode attributeList = new AttributeListNode(null, -1);
-    attributeList.items.remove(0);
-//    attributeList.hasOverride = true;
-//    attributeList.hasProtected = true;
-    attributeList.hasPublic = true;
-
     StatementListNode functionBody = new StatementListNode(null);
 
     QualifiedIdentifierNode name = new QualifiedIdentifierNode(null, "v1", 0);
@@ -66,39 +57,21 @@ public class ASTBuilder extends AbstractBuilder {
     VariableBindingNode var = new VariableBindingNode(null, null, Tokens.VAR_TOKEN, new TypedIdentifierNode(name, createTypeMemberExpressionNode(type), 0), new MemberExpressionNode(null, call, 0));
     functionBody.items.add(new VariableDefinitionNode(null, null, Tokens.VAR_TOKEN, new ListNode(null, var, 0), 0));
 
-
     GetExpressionNode getExpressionNode = new GetExpressionNode(new IdentifierNode("v1", false, 0));
     getExpressionNode.setMode(Tokens.EMPTY_TOKEN);
 
-    GetExpressionNode getExpressionNode2 = new GetExpressionNode(new IdentifierNode("addChild", false, 0));
-    getExpressionNode.setMode(Tokens.EMPTY_TOKEN);
-
     SetExpressionNode set = new SetExpressionNode(new IdentifierNode("text", 0), new ArgumentListNode(new LiteralStringNode("sss AEFLCWEHJCKSAX", false, false), 0));
-//    ExpressionStatementNode expressionStatementNode = new ExpressionStatementNode(new ListNode(null, new MemberExpressionNode(new MemberExpressionNode(null, getExpressionNode, 0), set, 0), 0));
     ExpressionStatementNode expressionStatementNode = new ExpressionStatementNode(new MemberExpressionNode(new MemberExpressionNode(null, getExpressionNode, 0), set, 0));
     functionBody.items.add(expressionStatementNode);
 
     CallExpressionNode callMethod = new CallExpressionNode(new IdentifierNode("addChild", false, 0), new ArgumentListNode(new MemberExpressionNode(null, getExpressionNode, 0), 0));
-//    CallExpressionNode callMethod = new CallExpressionNode(getExpressionNode2, new ArgumentListNode(new MemberExpressionNode(null, getExpressionNode, 0), 0));
     callMethod.setMode(Tokens.EMPTY_TOKEN);
-    callMethod.setRValue(true);
 
+    functionBody.items.add(new ExpressionStatementNode(new MemberExpressionNode(null, callMethod, 0)));
 
-//    if (statements.items.size() == 2  && s) {
-//      s = false;
-    functionBody.items.add(new ExpressionStatementNode(new ListNode(null, new MemberExpressionNode(null, callMethod, 0), 0)));
-//      functionBody.items.add(((FunctionDefinitionNode) statements.items.get(1)).fexpr.body.items.get(2));
-//      ASTUtil.dump(functionBody.items.last(), ((FunctionDefinitionNode) statements.items.get(1)).fexpr.body.items.get(2));
-//    }
-
-//    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, null, createChildrenFunctionSignature, functionBody, -1);
-//    functionCommon.setFunctionDefinition(true);
-//    FunctionDefinitionNode node = new FunctionDefinitionNode(context, nodeFactory.current_package, attributeList, createChildrenFunctionName, functionCommon);
-
-//    IdentifierNode identifier = new IdentifierNode("createChildren", false, -1);
-    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, createChildrenFunctionName.identifier, createChildrenFunctionSignature, functionBody, -1);
-    FunctionDefinitionNode node = new FunctionDefinitionNode(context, nodeFactory.current_package, attributeList, createChildrenFunctionName, functionCommon);
-    statements.items.add(node);
+    // use nodeFactory, we need internal_name generation
+    FunctionCommonNode functionCommon = nodeFactory.functionCommon(context, createChildrenFunctionName.identifier, createChildrenFunctionSignature, functionBody, 0);
+    statements.items.add(new FunctionDefinitionNode(context, nodeFactory.current_package, attributeListPublic, createChildrenFunctionName, functionCommon));
   }
 
   private static MemberExpressionNode createTypeMemberExpressionNode(IdentifierNode identifier) {
